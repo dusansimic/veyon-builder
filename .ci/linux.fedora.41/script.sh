ad6c624e73a8173b1937dadbfc4e60ded202ec88
Dušan Simić <dusan.simic1810@gmail.com>
2024-11-26 16:25:51 +0100

    fix: Remove workspace from paths

--- .ci/linux.fedora.41/script.sh
+++ .ci/linux.fedora.41/script.sh
@@ -4,9 +4,5 @@ set -e
 
 export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_PCH=OFF -DCPACK_DIST=fedora.41 -DCMAKE_CXX_FLAGS=-Wno-template-id-cdtor"
 
-__GITHUB_WORKSPACE=$1
-__VEYON_DIR=$2
-__TMP_DIR=$3
-
-veyon-builder/.ci/common/linux-build.sh ${__GITHUB_WORKSPACE}/${__VEYON_DIR} $__TMP_DIR
-veyon_builder/.ci/common/finalize-rpm.sh ${__GITHUB_WORKSPACE}/${__VEYON_DIR} $__TMP_DIR
+veyon-builder/.ci/common/linux-build.sh $@
+veyon_builder/.ci/common/finalize-rpm.sh $@
