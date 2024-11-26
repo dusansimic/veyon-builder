67227a7a6439a2fb8d0f0ec580c56bfa86b1deff
Dušan Simić <dusan.simic1810@gmail.com>
2024-11-26 16:51:49 +0100

    fix: Put repo in root

--- .ci/linux.fedora.41/script.sh
+++ .ci/linux.fedora.41/script.sh
@@ -4,5 +4,5 @@ set -ex
 
 export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_PCH=OFF -DCPACK_DIST=fedora.41 -DCMAKE_CXX_FLAGS=-Wno-template-id-cdtor"
 
-veyon-builder/.ci/common/linux-build.sh $@
-veyon_builder/.ci/common/finalize-rpm.sh $@
+.ci/common/linux-build.sh $@
+.ci/common/finalize-rpm.sh $@
