40796960cc99eccf55bfa36645ecd1683a785d43
Dušan Simić <dusan.simic1810@gmail.com>
2024-11-26 16:15:49 +0100

    fix: Refactor scripts

--- .ci/linux.fedora.41/script.sh
+++ .ci/linux.fedora.41/script.sh
@@ -4,5 +4,9 @@ set -e
 
 export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_PCH=OFF -DCPACK_DIST=fedora.41 -DCMAKE_CXX_FLAGS=-Wno-template-id-cdtor"
 
-$GITHUB_WORKSPACE/.ci/common/linux-build.sh $@
-$GITHUB_WORKSPACE/.ci/common/finalize-rpm.sh $1 $2
+__GITHUB_WORKSPACE=$1
+__VEYON_DIR=$2
+__TMP_DIR=$3
+
+${__GITHUB_WORKSPACE}/veyon-builder/.ci/common/linux-build.sh ${__GITHUB_WORKSPACE}/${__VEYON_DIR} $__TMP_DIR
+${__GITHUB_WORKSPACE}/veyon_builder/.ci/common/finalize-rpm.sh ${__GITHUB_WORKSPACE}/${__VEYON_DIR} $__TMP_DIR
